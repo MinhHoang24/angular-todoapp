bd185603f8b2fb4b164fbde2f42692ea7df40e3e
LittleMozart <hoangmp24@gmail.com>
2025-09-11 16:41:56 +0700

    store data on local storage

--- src/app/todo-list/todo-list.ts
+++ src/app/todo-list/todo-list.ts
@@ -13,23 +13,42 @@ import { TodoItem } from '../todo-item/todo-item';
 export class TodoList {
   tasks: { taskDes: string, status: string }[] = [];
 
+  constructor() {
+    if (typeof window !== 'undefined') {
+      const savedTasks = localStorage.getItem('tasks');
+      if (savedTasks) {
+        this.tasks = JSON.parse(savedTasks);
+      }
+    }
+  }
+
+  saveTasksToLocalStorage() {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+  }
+
   onTaskAdded(task: { taskDes: string, status: string }) {
     this.tasks.push(task);
     console.log(this.tasks);
+    this.saveTasksToLocalStorage();
   }
 
   onToggleStatus(task: { taskDes: string, status: string }) {
     task.status = task.status === 'unfinished' ? 'finished' : 'unfinished';
+    this.saveTasksToLocalStorage();
   }
 
   onUpdateTask(updatedTaskDescription: string, taskToUpdate: { taskDes: string, status: string }) {
     const task = this.tasks.find(t => t.taskDes === taskToUpdate.taskDes);
     if (task) {
       task.taskDes = updatedTaskDescription;
+      this.saveTasksToLocalStorage();
     }
   }
 
   onDeleteTask(taskToDelete: { taskDes: string }) {
     this.tasks = this.tasks.filter(task => task.taskDes !== taskToDelete.taskDes);
+    this.saveTasksToLocalStorage();
   }
 }
